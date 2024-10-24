a4ca211a217900dba6c0e10e0191baf6f60a0728
Mohammad Tauqeer Kondkari <mohammedtauqeer4oct@gmail.com>
2024-10-24 13:11:13 +0530

    commiting

--- main.py
+++ main.py
@@ -64,34 +64,33 @@ def index():
     return render_template("index.html")
 
 # Define a route for the home page
-@app.route('/predict', methods=['GET', 'POST'])
-def home():
-    if request.method == 'GET':
-        symptoms = request.form.get('symptoms')
-        # mysysms = request.form.get('mysysms')
-        # print(mysysms)
-        print(symptoms)
-        if symptoms =="Symptoms":
-            message = "Please either write symptoms or you have written misspelled symptoms"
-            return render_template('index.html', message=message)
-        else:
+@app.route('/predict', methods=['POST'])
+def predict():
+    symptoms_input = request.form.get('symptoms')
 
-            # Split the user's input into a list of symptoms (assuming they are comma-separated)
-            user_symptoms = [s.strip() for s in symptoms.split(',')]
-            # Remove any extra characters, if any
-            user_symptoms = [symptom.strip("[]' ") for symptom in user_symptoms]
-            predicted_disease = get_predicted_value(user_symptoms)
-            dis_des, precautions, medications, rec_diet, workout = helper(predicted_disease)
+    if not symptoms_input:
+        return render_template('index.html')  # Handle case if no symptoms are provided
 
-            my_precautions = []
-            for i in precautions[0]:
-                my_precautions.append(i)
+    symptoms = symptoms_input.lower().split(', ')
+    input_vector = [0] * len(symptoms_dict)
 
-            return render_template('index.html', predicted_disease=predicted_disease, dis_des=dis_des,
-                                   my_precautions=my_precautions, medications=medications, my_diet=rec_diet,
-                                   workout=workout)
+    for symptom in symptoms:
+        if symptom in symptoms_dict:
+            input_vector[symptoms_dict[symptom]] = 1
 
-    return render_template('index.html')
+    prediction = svc.predict([input_vector])[0]
+    predicted_disease = diseases_list[prediction]
+
+    desc, pre, med, die, wrkout = helper(predicted_disease)
+
+    return render_template('index.html', 
+                           predicted_disease=predicted_disease,
+                           dis_des=desc, 
+                           my_precautions=pre, 
+                           medications=med, 
+                           workout=wrkout, 
+                           my_diet=die)
+
 
 
 
